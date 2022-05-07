aa4b22f28e20d8082f9be517be44917a8541da76
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-05-08 01:27:16 +0900

    4まで

---
+++ WordList/FinishViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinishViewController.swift
+//  WordList
+//
+//  Created by JotaroSugiyama on 2022/05/08.
+//
+
+import UIKit
+
+class FinishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WordList/QuestionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  QuestionViewController.swift
+//  WordList
+//
+//  Created by JotaroSugiyama on 2022/05/08.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    
+    var isAnswered: Bool = false
+    var wordArray: [Dictionary<String,String>] = []
+    var nowNumber: Int = 0
+    let saveData = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        answerLabel.text = ""
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String,String>]
+        
+        wordArray.shuffle()
+        questionLabel.text = wordArray[nowNumber]["english"]
+    }
+    
+    @IBAction func nextButtonTapped(){
+        if isAnswered{
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            if nowNumber < wordArray.count{
+                questionLabel.text = wordArray[nowNumber]["english"]
+                isAnswered = false
+                nextButton.setTitle("答えを表示", for: .normal)
+            }else{
+                nowNumber = 0
+                nextButton.setTitle("次へ", for: .normal)
+            }
+        }
+    }
+
+}
